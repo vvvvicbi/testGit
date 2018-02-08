211d11b1492c9db04213dad158c1ab3da64accf7
vvvvicbi <bi_haibin@163.com>
2018-02-08 18:14:08 +0800

    First Commit

---
+++ testGitDemo/testGitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testGitDemo
+//
+//  Created by VicBi on 2018/2/8.
+//  Copyright © 2018年 VicBi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
